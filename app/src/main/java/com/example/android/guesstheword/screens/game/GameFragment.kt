d465f5f9ac64fc9cdad0c300480b40cf2319c4b6
Praveen <pkchmits@gmail.com>
2020-10-02 05:00:38 +0530

    Done

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -55,19 +55,14 @@ class GameFragment : Fragment() {
         // Set the viewmodel for databinding - this allows the bound layout access to all of the
         // data in the VieWModel
         binding.gameViewModel = viewModel
-        // TODO (02) Call binding.setLifecycleOwner and pass in "this" fragment to make the
+        // DONE (02) Call binding.setLifecycleOwner and pass in "this" fragment to make the
         // data binding lifecycle aware
+        binding.setLifecycleOwner(this)
 
-        // TODO (03) Now you can remove the score and word observers - we'll fix
+        // DONE (03) Now you can remove the score and word observers - we'll fix
         // the currentTime observation in the next step
         /** Setting up LiveData observation relationship **/
-        viewModel.word.observe(this, Observer { newWord ->
-            binding.wordText.text = newWord
-        })
 
-        viewModel.score.observe(this, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
 
         viewModel.currentTime.observe(this, Observer { newTime ->
             binding.timerText.text = DateUtils.formatElapsedTime(newTime)
