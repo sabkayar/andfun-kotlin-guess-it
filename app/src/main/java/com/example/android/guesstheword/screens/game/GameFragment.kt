713867b2aae1f4b2f2ea27578d0108357150b836
Praveen <pkchmits@gmail.com>
2020-10-02 04:18:27 +0530

    Added databinding to ViewModel Here viewModel is treated as dataclass

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -52,14 +52,11 @@ class GameFragment : Fragment() {
         // Get the viewmodel
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        // TODO (03) Pass the GameViewModel into the data binding - then you can remove the
+        // DONE (03) Pass the GameViewModel into the data binding - then you can remove the
         // OnClickListener setup from here
-        binding.correctButton.setOnClickListener {
-            viewModel.onCorrect()
-        }
-        binding.skipButton.setOnClickListener {
-            viewModel.onSkip()
-        }
+
+        binding.gameViewModel = viewModel
+
 
         /** Setting up LiveData observation relationship **/
         viewModel.word.observe(this, Observer { newWord ->
